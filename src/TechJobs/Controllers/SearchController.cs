ea1afeb8e18c3a5b1a7a01119571a3beca667dc3
Lisa Ruby <lruby1203@hotmail.com>
2017-03-20 23:18:31 -0500

    Competed assignment. No bonus missions included.

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -13,8 +13,26 @@ namespace TechJobs.Controllers
             return View();
         }
 
-        // TODO #1 - Create a Results action method to process 
+        // Results action method to process 
         // search request and display results
+        
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
 
+            ViewBag.columns = ListController.columnChoices;
+            if (searchType == "all")
+            {
+               jobs = JobData.FindByValue(searchTerm);
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+            ViewBag.jobs = jobs;
+            return View("Views/Search/Index.cshtml");
+
+            
+        }
     }
 }
